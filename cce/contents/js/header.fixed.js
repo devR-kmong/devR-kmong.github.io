28d366cf283b667416a9c8608df9f5977d86d98a
devR <102127654+devR-kmong@users.noreply.github.com>
2022-08-27 23:46:36 +0900

    Add files via upload

---
+++ cce/contents/js/header.fixed.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    var Offset = $('.header').offset();
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > Offset.top) {
+            $('.header').addClass('Fixed');
+        }
+        else {
+            $('.header').removeClass('Fixed');
+        }
+    });
+});
