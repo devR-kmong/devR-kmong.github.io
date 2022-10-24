6de1307da3c58e06e864cd34f124b0e176f1dd9f
devR <102127654+devR-kmong@users.noreply.github.com>
2022-10-24 11:52:13 +0900

    Delete debec directory

--- debec/js/login.js
+++
@@ -1,7 +0,0 @@
-$('.loginBtn').click(function(){
-    $('.login-wrapper').slideToggle(70).css('display','flex');
-})
-
-$('.login-closeBtn').click(function(){
-    $('.login-wrapper').slideUp(70).css('display','none');
-})
