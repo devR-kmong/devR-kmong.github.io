dd580a0875fdacdcf3c9c6a5b61b1432e8f3240e
devR <102127654+devR-kmong@users.noreply.github.com>
2022-10-31 18:45:26 +0900

    Add files via upload

---
+++ cloverse/js/header.fixed.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    var Offset = $('header').offset();
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > Offset.top) {
+            $('header').addClass('Fixed');
+        }
+        else {
+            $('header').removeClass('Fixed');
+        }
+    });
+});
+
+
+
+//////////////////////////////////////////////////////////
+$(document).ready(function () {
+    var Offset = $('header').offset();
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > Offset.top) {
+            $('.bar1, .bar2, .bar3').addClass('bar-color');
+        }
+        else {
+            $('.bar1, .bar2, .bar3').removeClass('bar-color');
+        }
+    });
+});
