1b28504c30a67466801e4e5b1a664206bf5458d6
devR <102127654+devR-kmong@users.noreply.github.com>
2023-03-08 19:30:03 +0900

    Add files via upload

--- zimssabell/contents/js/common.js
+++ zimssabell/contents/js/common.js
@@ -1,7 +1,7 @@
 new WOW({
     boxClass:     'wow',
     animateClass: 'animated',
-    offset:       300,
+    offset:       200,
     mobile:       true,
     live:         true
 }).init();
@@ -31,9 +31,9 @@ $(function () {
     $(function () {
         $(window).scroll(function () {
             if ($(this).scrollTop() >= 100) {
-                $('.floating').fadeIn().css('display', 'flex');
+                $('.floating').fadeIn(100).css('display', 'flex');
             } else {
-                $('.floating').fadeOut();
+                $('.floating').fadeOut(100);
             }
         });
     });
