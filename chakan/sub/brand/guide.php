0d03e2934cf52ecf452e958e006e3a00b64a10ea
devR <102127654+devR-kmong@users.noreply.github.com>
2023-09-27 12:28:43 +0900

    Add files via upload

---
+++ chakan/sub/brand/guide.php
@@ -0,0 +1,20 @@
+<?php
+include_once('../common.php');
+
+$g5['title'] = "운영안내";
+
+add_stylesheet('<link rel="stylesheet" href="'.G5_URL.'/sub/contents/css/sub-promotion.css">', 0);
+add_stylesheet('<link rel="stylesheet" href="'.G5_URL.'/sub/contents/css/guide.css">', 1);
+
+include_once(G5_PATH.'/head.php');
+?>
+
+
+
+
+
+
+
+<?php
+include_once(G5_PATH.'/tail.php');
+?>
---
+++ chakan/sub/franchise/process.php
@@ -0,0 +1,20 @@
+<?php
+include_once('../common.php');
+
+$g5['title'] = "창업절차";
+
+add_stylesheet('<link rel="stylesheet" href="'.G5_URL.'/sub/contents/css/sub-promotion.css">', 0);
+add_stylesheet('<link rel="stylesheet" href="'.G5_URL.'/sub/contents/css/process.css">', 0);
+
+include_once(G5_PATH.'/head.php');
+?>
+
+
+
+
+
+
+
+<?php
+include_once(G5_PATH.'/tail.php');
+?>
