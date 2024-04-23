354d3d22d0dd705b82854cbb925e9c7abdcf00c6
devR <102127654+devR-kmong@users.noreply.github.com>
2024-04-24 01:46:30 +0900

    Add files via upload

---
+++ src/js/common.js
@@ -0,0 +1,79 @@
+AOS.init({
+    once: true,
+    delay: 100,
+    offset: 200,
+    duration: 1200,
+});
+
+
+
+$(function () {
+    var Offset = $('header').offset();
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > Offset.top) {
+            $('header').addClass('Fixed');
+        }
+        else {
+            $('header').removeClass('Fixed');
+        }
+    });
+});
+
+
+
+
+$(window).on('scroll', function () {
+    $('section').each(function () {
+        if ($(window).scrollTop() >= $(this).offset().top - 500) {
+            var id = $(this).attr('id');
+            $('nav ul li a').removeClass('actives');
+            $('nav ul li a[href=#' + id + ']').addClass('actives');
+        }
+    });
+});
+
+
+
+$(function () {
+    $('.logo, nav ul li a').click(function (e) {
+        $.scrollTo(this.hash || 0, 500);
+        e.preventDefault();
+    });
+});
+
+
+
+$('.menuBtns').click(function () {
+    $(this).toggleClass('change');
+});
+
+$('nav ul li a').click(function () {
+    $('#check').prop('checked', false);
+    $('.menuBtns').removeClass('change');
+});
+
+$('.overlay').click(function () {
+    $('#check').prop('checked', false);
+    $('.menuBtns').removeClass('change');
+});
+
+
+
+if (navigator.userAgent.indexOf("Trident") > 0) {
+    alert("이 사이트에 대한 Internet Explorer 지원이 종료되었습니다. " +
+        "확인 버튼을 누르면 Edge 브라우저로 이동합니다.")
+    window.location = 'microsoft-edge:' + window.location.href;
+};
+
+
+
+document.addEventListener("mousemove", parallax);
+function parallax(event) {
+    this.querySelectorAll(".p-image").forEach((shift) => {
+        const position = shift.getAttribute("value");
+        const x = (window.innerWidth - event.pageX * position) / 90;
+        const y = (window.innerHeight - event.pageY * position) / 90;
+
+        shift.style.transform = `translateX(${x}px) translateY(${y}px)`;
+    });
+}
