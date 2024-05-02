90113c92f3cf748c000dd8f05a2d54e760d30053
devR <102127654+devR-kmong@users.noreply.github.com>
2024-05-02 18:24:06 +0900

    Add files via upload

--- src/js/car.js
+++ src/js/car.js
@@ -3,8 +3,8 @@ let car = document.getElementById('car');
 window.addEventListener('scroll', function () {
     let value = window.scrollY;
 
-    if (value >= 3700) {
-        car.style.right = (value - 3700) * 1.5 - 500 + 'px';
+    if (value >= 3400) {
+        car.style.right = (value - 3400) * 1.5 - 500 + 'px';
     }
 
     if (window.innerWidth <= 1250) {
