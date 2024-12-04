dddce42af14ebae00e2ac7634092ffdadbab0529
devR <102127654+devR-kmong@users.noreply.github.com>
2024-12-04 17:07:06 +0900

    Add files via upload

--- busters/js/common.js
+++ busters/js/common.js
@@ -6,33 +6,6 @@ AOS.init({
 
 
 
-$(function () {
-    $('.floating').hide();
-    
-    $(function () {
-        $(window).scroll(function () {
-            if ($(this).scrollTop() > 500) {
-                $('.floating').fadeIn();
-            } else {
-                $('.floating').fadeOut();
-            }
-        });
-    });
-});
-
-
-
-$(window).on('scroll', function () {
-    $('section').each(function () {
-        if ($(window).scrollTop() >= $(this).offset().top - 500) {
-            var id = $(this).attr('id');
-            $('nav ul li a').removeClass('actives');
-            $('nav ul li a[href=#' + id + ']').addClass('actives');
-        }
-    });
-});
-
-
 $(window).on('scroll', function () {
     $('.article').each(function () {
         if ($(window).scrollTop() >= $(this).offset().top - 500) {
@@ -46,21 +19,11 @@ $(window).on('scroll', function () {
 
 
 $(function () {
-    $('.topBtn, .remoteBtn, .logo, .logo-w, nav ul li a').click(function (e) {
+    $('.remoteBtn, .logo').click(function (e) {
         $.scrollTo(this.hash || 0, 500);
         e.preventDefault();
     });
 });
-
-
-
-if (window.innerWidth > 1600) {
-    $('header').on('scroll touchmove mousewheel', function (event) {
-        event.preventDefault();
-        event.stopPropagation();
-        return false;
-    })
-}  
 
 
 
@@ -94,3 +57,57 @@ if (navigator.userAgent.indexOf("Trident") > 0) {
         "확인 버튼을 누르면 Edge 브라우저로 이동합니다.")
     window.location = 'microsoft-edge:' + window.location.href;
 }
+
+
+$(function () {
+    $(window).scroll(function () {
+
+        var here = $(".article2").offset().top;
+        var height = $(document).scrollTop();
+
+        if (here > height) {
+            $('.menu').removeClass('on');
+        }
+        else {
+            $('.menu').addClass('on');
+        }
+    })
+});
+
+
+$(document).on("keyup", ".phoneNumber", function () {
+    $(this).val($(this).val().replace(/[^0-9]/g, "").replace(/(^02|^0505|^1[0-9]{3}|^0[0-9]{2})([0-9]+)?([0-9]{4})$/, "$1-$2-$3").replace("--", "-"));
+});
+
+
+$('#personalPopup').click(function () {
+    $('.pp-wrapper').fadeIn().css('display','flex');
+    $('.policy-popup textarea').scrollTop(0);
+});
+
+$('.pp-closeBtn').click(function () {
+    $('.pp-wrapper').fadeOut();
+});
+
+$('.f-personal-text').click(function () {
+    if ($('#cb').is(':checked')) {
+        $('.f-personal').addClass('f-personal-no');
+        $('.f-personal-text p').html('개인정보처리방침에 동의해주세요.');
+        $('.gform .submitBtn').attr('disabled', true);
+    } else {
+        $('.f-personal').removeClass('f-personal-no');
+        $('.f-personal-text p').html('개인정보수집 및 이용에 동의합니다.');
+        $('.gform .submitBtn').attr('disabled', false);
+    }
+});
+
+
+
+$(document).on(
+    'keydown',
+    function (event) {
+        if (event.key == "Escape") {
+            $('.pp-wrapper').fadeOut();
+        }
+    }
+);
