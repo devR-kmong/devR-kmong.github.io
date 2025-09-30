0b03cf05079779553c38de8a43123a45dedc6fcf
devR <102127654+devR-kmong@users.noreply.github.com>
2025-09-30 11:55:57 +0900

    Add files via upload

---
+++ gi/contents/js/common.js
@@ -0,0 +1,61 @@
+if (window.innerWidth > 768) {
+    AOS.init({
+        once: true,
+        delay: 100,
+        offset: 200,
+        duration: 1200,
+    })
+} else {
+    AOS.init({
+        once: true,
+        delay: 50,
+        offset: 100,
+        duration: 1200,
+    })
+}
+
+
+
+$(function () {
+    var Offset = $('.header').offset();
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > Offset.top) {
+            $('.header').addClass('Fixed');
+            $('.logo-w').addClass('active');
+            $('.logo').removeClass('active');
+            $('.menu li a').addClass('Fixed2');
+            $('.bar1, .bar2, .bar3').addClass('bar-color');
+        }
+        else {
+            $('.header').removeClass('Fixed');
+            $('.logo-w').removeClass('active');
+            $('.logo').addClass('active');
+            $('.menu li a').removeClass('Fixed2');
+            $('.bar1, .bar2, .bar3').removeClass('bar-color');
+        }
+    });
+});
+
+
+
+$('.menuBtns').click(function () {
+    $(this).toggleClass('change');
+})
+
+$('.menu li a').click(function () {
+    $('#check').prop('checked', false);
+    $('.menuBtns').removeClass('change');
+})
+
+$('.overlay').click(function () {
+    $('#check').prop('checked', false);
+    $('.menuBtns').removeClass('change');
+})
+
+
+
+if (navigator.userAgent.indexOf("Trident") > 0) {
+    alert("이 사이트에 대한 Internet Explorer 지원이 종료되었습니다. " +
+        "확인 버튼을 누르면 Edge 브라우저로 이동합니다.")
+    window.location = 'microsoft-edge:' + window.location.href;
+}
