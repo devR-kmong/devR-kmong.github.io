1a634bf8310a91aae029171fdf65dd98ed30a921
devR <102127654+devR-kmong@users.noreply.github.com>
2025-10-30 18:46:43 +0900

    Add files via upload

---
+++ gi/sub/about/us.php
@@ -0,0 +1,20 @@
+<?php
+include_once('../../common.php');
+
+$g5['title'] = "회사개요";
+
+add_stylesheet('<link rel="stylesheet" href="'.G5_URL.'/sub/contents/css/sub_common.css">', 0);
+add_stylesheet('<link rel="stylesheet" href="'.G5_URL.'/sub/contents/css/us.css">', 1);
+
+include_once(G5_PATH.'/head.php');
+?>
+
+
+
+
+
+
+
+<?php
+include_once(G5_PATH.'/tail.php');
+?>
