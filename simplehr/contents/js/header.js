0c5352a84c51ad79aece89251e73468f0803e9d6
devR <102127654+devR-kmong@users.noreply.github.com>
2025-11-29 16:34:37 +0900

    Add files via upload

---
+++ simplehr/contents/js/header.js
@@ -0,0 +1,79 @@
+$(function () {
+    $('.header').addClass('trans');
+})
+
+$(function () {
+    var Offset = $('.header').offset();
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > Offset.top) {
+            $('.header').addClass('Fixed');
+            $('.logo-w').addClass('hide');
+            $('.logo').removeClass('hide');
+            $('.menu-item a').addClass('on');
+        }
+        else {
+            $('.header').removeClass('Fixed');
+            $('.logo-w').removeClass('hide');
+            $('.logo').addClass('hide');
+            $('.menu-item a').removeClass('on');
+            $('.mi-head').removeClass('on');
+        }
+    });
+});
+
+
+
+if (window.innerWidth > 1400) {
+    $(function () {    
+        for (let i=0; i<$('.menu-item').length; i++) {
+            $('.menu-item').eq(i).mouseenter(function () {
+                $('.mh-' + i).show().css('display','flex');
+            })
+    
+            $('.menu-item').eq(i).mouseleave(function () {
+                $('.mh-' + i).hide();
+            })
+        }
+    })
+    
+    $(document).bind("mouseleave", function(){
+        $('.menu-hover').hide();
+    });
+}
+
+
+
+if (window.innerWidth <= 1400) {
+    let mih = $('.mi-head');
+    let mhv = $('.menu-hover');
+    let arrow = $('.mi-head i');
+
+    mih.click(function (e) {
+        e.preventDefault();
+    })
+
+    for (let i = 0; i < mih.length; i++) {
+        mih.eq(i).click(function () {
+            mhv.not(mhv.eq(i)).removeClass('on');
+            arrow.not(arrow.eq(i)).removeClass('i-rotate');
+            mhv.eq(i).toggleClass('on');
+            arrow.eq(i).toggleClass('i-rotate');
+        })
+    }
+}
+
+
+
+$(function () {
+    $('.toggleBtn').click(function () {
+        $('body').addClass('on');
+        $('.menu').show();
+        $('.closeBtn').show();
+    })
+
+    $('.closeBtn').click(function () {
+        $('body').removeClass('on');
+        $(this).hide();
+        $('.menu').hide();
+    })
+})
---
+++ simplehr/sub/contents/js/sub_common.js
@@ -0,0 +1,3 @@
+$(function () {
+    $('.header').removeClass('trans');
+})
