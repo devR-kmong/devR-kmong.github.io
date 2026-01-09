23767976110e94692dd3184b9b0a6de40cac0f9a
devR <102127654+devR-kmong@users.noreply.github.com>
2026-01-09 14:22:31 +0900

    Add files via upload

--- gaia/contents/js/gsap.js
+++ gaia/contents/js/gsap.js
@@ -1,22 +1,37 @@
 gsap.registerPlugin(ScrollTrigger);
 
 const promotion = gsap.timeline();
+let mm = gsap.matchMedia();
 
-promotion.from(".logo", { fontSize: "250px" })
+mm.add({
+    isDesktop: "(min-width: 992px)",
+    isMobile: "(max-width: 991px)"
+}, (context) => {
+    let { isDesktop, isMobile } = context.conditions;
 
-ScrollTrigger.create({
-    animation: promotion,
-    trigger: ".promotion",
-    start: "top top",
-    end: "bottom",
-    scrub: true,
+    promotion.from(".logo", {
+        fontSize: isMobile ? "150px" : "250px",
+        y: isMobile ? "15%" : "0"
+    });
+
+    ScrollTrigger.create({
+        animation: promotion,
+        trigger: ".promotion",
+        start: "top top",
+        end: "bottom",
+        scrub: true,
+    });
+
+    return () => {
+        promotion.kill();
+    };
 });
 
 
 [1, 2, 3, 4].forEach((num) => {
     const tl = gsap.timeline();
 
-    tl.from(`.a${num}-bg`, { scale: 1.05, autoAlpha: 0.1 })
+    tl.from(`.a${num}-bg`, { scale: 1.2, autoAlpha: 0.1 })
         .from(`.article${num} .at-text`, { y: 80, ease: 'none', autoAlpha: 0 }, "<")
         .to(`.article${num} .at-text h5`, { autoAlpha: 1 })
 
